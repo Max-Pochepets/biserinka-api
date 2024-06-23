9eab9dc1c736d30be930bc6705b12f3464edb12a
Max Pochepets <agreed_note@hotmail.com>
2024-06-23 03:28:34 +0300

    init spring boot

---
+++ src/main/java/com/kalinovna/biserinka/api/BiserinkaApplication.java
@@ -0,0 +1,12 @@
+package com.kalinovna.biserinka.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiserinkaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BiserinkaApplication.class, args);
+  }
+}
